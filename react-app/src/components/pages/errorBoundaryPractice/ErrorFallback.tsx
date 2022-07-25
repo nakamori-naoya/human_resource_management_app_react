0ca33740cea90514c9c9c30469f4e4bb1d1fb30d
NaoyaNakamori <73022482+nakamori-naoya@users.noreply.github.com>
2022-07-25 22:09:46 +0900

    feat:resetErrorBoundaryの追加

--- react-app/src/components/pages/errorBoundaryPractice/ErrorFallback.tsx
+++ react-app/src/components/pages/errorBoundaryPractice/ErrorFallback.tsx
@@ -1,10 +1,15 @@
 import { FallbackProps } from 'react-error-boundary';
 
-const ErrorFallback = ({ error }: FallbackProps) => {
+const ErrorFallback = ({ error, resetErrorBoundary }: FallbackProps) => {
   return (
     <div>
       <h2>エラーが発生しました。</h2>
       <pre>{error.message}</pre>
+      {/* resetErrorBoundaryは、呼び出されるとエラー境界の状態をリセット。
+      おそらくErrorBoundaryの内側のコンポーネントを再レンダリングする */}
+      <button type='button' onClick={resetErrorBoundary}>
+        もう一度、実行する
+      </button>
     </div>
   );
 };
