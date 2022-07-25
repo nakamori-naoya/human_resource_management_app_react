65c1b727a6400ea1e62202c2bab877522fb37cd3
NaoyaNakamori <73022482+nakamori-naoya@users.noreply.github.com>
2022-07-25 23:19:44 +0900

    feat:エラーが起きた際にリセットを行う処理を追加

--- react-app/src/components/pages/errorBoundaryPractice/ErrorBoundaryPractice.tsx
+++ react-app/src/components/pages/errorBoundaryPractice/ErrorBoundaryPractice.tsx
@@ -3,6 +3,7 @@ import ErrorFallback from './ErrorFallback';
 import MightThrowError from './MightThrowError';
 import Spineer from '../../ui-elements/Spinner';
 import { Suspense } from 'react';
+import { useQueryErrorResetBoundary } from '@tanstack/react-query';
 
 const ErrorBoundaryPractice = () => {
   const onError = (error: Error, info: { componentStack: string }) => {
@@ -10,10 +11,12 @@ const ErrorBoundaryPractice = () => {
     console.log('info.componentStack:', info.componentStack); //componentStackには、エラーをthrowしたcomponentのチェーン(当該コンポーネントと親コンポーネント群)が出力される
   };
 
+  const { reset } = useQueryErrorResetBoundary();
+
   return (
     // onErrorには、エラーが発生した時に呼ばれる関数をコールバック関数として渡す
     <>
-      <ErrorBoundary FallbackComponent={ErrorFallback} onError={onError}>
+      <ErrorBoundary FallbackComponent={ErrorFallback} onError={onError} onReset={reset}>
         <Suspense fallback={<Spineer />}>
           <span>ErrorBoundaryPractice</span>
           <MightThrowError />
