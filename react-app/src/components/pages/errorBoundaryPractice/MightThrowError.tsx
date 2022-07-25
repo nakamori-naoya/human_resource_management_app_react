b095d7a5bee5d3c47c2565eba5418351e307ea44
NaoyaNakamori <73022482+nakamori-naoya@users.noreply.github.com>
2022-07-25 22:10:16 +0900

    feat:非同期処理を追加

--- react-app/src/components/pages/errorBoundaryPractice/MightThrowError.tsx
+++ react-app/src/components/pages/errorBoundaryPractice/MightThrowError.tsx
@@ -1,9 +1,18 @@
+import { useQuery } from '@tanstack/react-query';
+import { fetchMembers } from '../../../lib/private_app/members';
 const MightThrowError = () => {
+  const { data: users = [] } = useQuery(['members'], () => fetchMembers());
+
   if (new Date().getSeconds() % 2 === 0) {
     throw new Error('MightThrowError Component throw Error!!');
   }
 
-  return <p>safe!! not throw error!!</p>;
+  return (
+    <>
+      エンジニアメンバーは{users.length}人です。
+      <p>safe!! not throw error!!</p>;
+    </>
+  );
 };
 
 export default MightThrowError;
