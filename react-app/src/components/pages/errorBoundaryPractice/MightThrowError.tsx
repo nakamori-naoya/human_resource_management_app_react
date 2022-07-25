b08e494377a51763c43b54cebdc49adc142d3826
NaoyaNakamori <73022482+nakamori-naoya@users.noreply.github.com>
2022-07-25 21:47:48 +0900

    feat:文言等を微修正

--- react-app/src/components/pages/errorBoundaryPractice/MightThrowError.tsx
+++ react-app/src/components/pages/errorBoundaryPractice/MightThrowError.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
-
 const MightThrowError = () => {
-  if (new Date().getMilliseconds() % 2 === 0) {
-    throw new Error('Page3Child Throw Error');
+  if (new Date().getSeconds() % 2 === 0) {
+    throw new Error('MightThrowError Component throw Error!!');
   }
 
-  return <p>not throw error</p>;
+  return <p>safe!! not throw error!!</p>;
 };
 
 export default MightThrowError;
