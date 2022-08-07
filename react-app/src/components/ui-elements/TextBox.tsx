07ea0cf91ac378bd37a48b221c6cd59210b737a5
NaoyaNakamori <73022482+nakamori-naoya@users.noreply.github.com>
2022-08-08 00:25:00 +0900

    feat: emailコンポーネントのstate管理を追加
    後続のコミットで、カスタムフックの使用する

--- react-app/src/components/ui-elements/TextBox.tsx
+++ react-app/src/components/ui-elements/TextBox.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 type Props = {
   label: string;
   placeholder: string;
@@ -6,25 +7,30 @@ type Props = {
   type: string;
   autoComplete: string;
   required: boolean;
+  text: string;
+  setText: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
-
-const TextBox = ({ label, placeholder, id, name, type, autoComplete, required }: Props) => {
-  return (
-    <div>
-      <label htmlFor={id} className='sr-only'>
-        {label}
-      </label>
-      <input
-        id={id}
-        name={name}
-        type={type}
-        autoComplete={autoComplete} //https://omkz.net/form-autocomplete/
-        required={required}
-        className='relative block h-8 w-80 rounded-md border border-gray-300 px-3 py-2 text-black  placeholder:text-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm'
-        placeholder={placeholder}
-      />
-    </div>
-  );
-};
+const TextBox = React.memo(
+  ({ label, placeholder, id, name, type, autoComplete, required, text, setText }: Props) => {
+    return (
+      <div>
+        <label htmlFor={id} className='sr-only'>
+          {label}
+        </label>
+        <input
+          value={text}
+          onChange={(event) => setText(event)}
+          id={id}
+          name={name}
+          type={type}
+          autoComplete={autoComplete} //https://omkz.net/form-autocomplete/
+          required={required}
+          className='relative block h-8 w-80 rounded-md border border-gray-300 px-3 py-2 text-black  placeholder:text-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm'
+          placeholder={placeholder}
+        />
+      </div>
+    );
+  },
+);
 
 export default TextBox;
