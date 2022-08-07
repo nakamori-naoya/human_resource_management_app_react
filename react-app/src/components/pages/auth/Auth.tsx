6d92a1e967bc5df757e4c5ac4adea4cb843bc4d9
NaoyaNakamori <73022482+nakamori-naoya@users.noreply.github.com>
2022-08-08 01:05:01 +0900

    refactor: コンポーネント名の変更を行ったので、import名も変更

--- react-app/src/components/pages/auth/Auth.tsx
+++ react-app/src/components/pages/auth/Auth.tsx
@@ -1,12 +1,12 @@
-import EmailLogin from './EmailLogin';
+import EmailAuth from './EmailAuth';
 import OAuthLogin from './OAuthLogin';
 const Auth = () => {
   return (
     <div className='w-112 h-112 rounded-4xl mx-auto  mt-12 border shadow-md'>
       <div className='m-auto mt-10 block w-80'>
-        <EmailLogin />
+        <EmailAuth />
         <div className='mx-auto my-6 w-12'>または</div>
-        <OAuthLogin></OAuthLogin>
+        <OAuthLogin />
       </div>
     </div>
   );
