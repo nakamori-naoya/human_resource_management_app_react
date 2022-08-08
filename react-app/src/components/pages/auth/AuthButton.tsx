210e3b7c83befad253f5c7a0866c94197070a58d
NaoyaNakamori <73022482+nakamori-naoya@users.noreply.github.com>
2022-08-09 00:29:14 +0900

    feat:ログインとサインアップをコンポーネントから呼び出し可能にしました

--- react-app/src/components/pages/auth/AuthButton.tsx
+++ react-app/src/components/pages/auth/AuthButton.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 type Props = {
   label: string;
+  onClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => Promise<void>;
 };
 
-const AuthButton = ({ label }: Props) => {
+const AuthButton = ({ label, onClick }: Props) => {
   return (
     <button
+      onClick={onClick}
       type='submit'
       className='rounded-4xl flex h-9 w-full justify-center border border-transparent bg-indigo-400 py-1  px-4  text-xl font-extrabold  text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
     >
