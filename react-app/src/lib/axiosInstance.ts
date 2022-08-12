e1371aec1e7322b72c745a0a654a6723721781ea
NaoyaNakamori <73022482+nakamori-naoya@users.noreply.github.com>
2022-08-13 00:33:08 +0900

    fix: test環境では、applyCaseMiddlewareが動かなくなるため修正

--- react-app/src/lib/axiosInstance.ts
+++ react-app/src/lib/axiosInstance.ts
@@ -5,13 +5,26 @@ const options = {
   ignoreHeaders: true,
 };
 
-export const axiosInstance = applyCaseMiddleware(
-  axios.create({
-    baseURL: 'http://localhost:9001/api/v1',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    timeout: 2000,
-  }),
-  options,
-);
+
+// NOTE: test環境では、applyCaseMiddlewareが動かないため、条件分岐を行なっている
+// test環境では、axiosはMockを使い、レスポンスの制御を行なっているので、問題ないと思われる
+export const axiosInstance =
+  process.env.NODE_ENV === 'test'
+    ? axios.create({
+        baseURL: 'http://localhost:9001/api/v1',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        timeout: 2000,
+      })
+    : applyCaseMiddleware(
+        axios.create({
+          baseURL: 'http://localhost:9001/api/v1',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          timeout: 2000,
+        }),
+        options,
+      );
+
