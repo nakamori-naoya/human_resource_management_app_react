05c3c7cb71de035aa92675e474e9c68bd88bc7a2
NaoyaNakamori <73022482+nakamori-naoya@users.noreply.github.com>
2022-08-13 00:33:35 +0900

    rename:file

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ErrorBoundaryPractice from './components/pages/errorBoundaryPractice/ErrorBoundaryPractice';
-import Auth from './components/pages/auth/Auth';
+import AuthPresenter from './components/pages/auth/AuthPresenter';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Auth />} />
+          <Route path='/' element={<AuthPresenter />} />
           <Route path='/error-boundary-practice' element={<ErrorBoundaryPractice />} />
         </Routes>
       </BrowserRouter>
