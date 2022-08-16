8f9c90a50b2f64a4aab1538327e711066ef32172
NaoyaNakamori <73022482+nakamori-naoya@users.noreply.github.com>
2022-08-16 22:51:37 +0900

    feat:toastサービスの作成

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,10 +2,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ErrorBoundaryPractice from './components/pages/errorBoundaryPractice/ErrorBoundaryPractice';
 import AuthPresenter from './components/pages/auth/AuthPresenter';
 import TopPage from './components/pages/topPage/TopPage';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
     <>
+      <ToastContainer />
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<AuthPresenter />} />
